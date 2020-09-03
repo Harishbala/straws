be84fd8260373209a02eb26cb58e07bdcf4dcc10
Harish Bala <b.harish85@gmail.com>
2020-09-03 00:06:08 -0400

    graph lib cleanup

--- graph_lib/graph.cpp
+++ graph_lib/graph.cpp
@@ -22,13 +22,4 @@ void Graph::print_nodes() const
       std::cout<<'\n';
   }
 }
-int main()
-{
-    Graph graph;
-    graph.insert_node(1, {2, 3, 4});
-    graph.insert_node(2, {3, 4, 1});
-    graph.insert_node(3, {4, 3, 2});
-    graph.insert_node(4, {1, 3, 2});
 
-    graph.print_nodes();
-}
