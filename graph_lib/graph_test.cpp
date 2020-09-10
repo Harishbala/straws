0eec5d6bb0b38e095d79d276817ff1745129d314
Harish Bala <b.harish85@gmail.com>
2020-09-10 18:28:15 -0400

    Added unit test framework for graphlib

--- graph_lib/graph_test.cpp
+++ graph_lib/graph_test.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <gtest/gtest.h>
 
 #include "graph.hpp"
 #include "graph_as_dot.hpp"
 
 using nlohmann::json;
 
-int main()
+TEST(GraphTest, Graph_Collective_Test)
 {
     Graph<std::string> graph;
 
@@ -20,17 +21,17 @@ int main()
     }
 
     graph.add_edge("Aristotle", "Kant", 25);
-    //graph.print_nodes_as_dot();
-    //graph.delete_node("Descartes");
 
     int count  = 1;
     GraphAsDot<std::string> graph_dot;
     graph.bfs("Plato", [&graph_dot, &count](EdgeTraversal<std::string>& et){
-            //std::cout<<et.parent_<<" -> "<<et.weight<<" -> "<<et.current_ << '\n';
-            //bfs_graph.add_edge(et.parent_, et.current_, et.weight);
             graph_dot.add_edge(et.parent_, et.current_, count++);
             });
     std::cout << graph_dot.str();
     graph.dijikstra_shortest_paths("Plato");
-    //bfs_graph.print_nodes_as_dot();
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
