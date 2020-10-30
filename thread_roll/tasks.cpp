85affbba96f5530b8aefb9a41f83bd525d301303
Harish Bala <b.harish85@gmail.com>
2020-10-30 13:32:11 -0400

    Added future wait_for

--- thread_roll/tasks.cpp
+++ thread_roll/tasks.cpp
@@ -1,10 +1,26 @@
+// Practical experiments based on Scott Meyers Effective Modern C++ book.
+
 #include <iostream>
 #include <future>
+#include <chrono>
+
+using namespace std::literals;
 
 int task() {
     return 0;
 }
+
 int main() {
     auto future_var = std::async(task);
+
+    auto fut2 = std::async(std::launch::async |
+                            std::launch::deferred, task);
+
     std::cout << "Task returned:" << future_var.get() << std::endl;
+
+    std::cout << "Task2 returned:" << fut2.get() << std::endl;
+
+    while (fut2.wait_for(100ms) !=  std::future_status::ready)
+    {
+    }
 }
